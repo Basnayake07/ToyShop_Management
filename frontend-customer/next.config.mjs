30c1b03e227a56180e499c56bb52e212c530ffd9
SunaliB <basnaya-im21009@stu.kln.ac.lk>
2025-04-25 10:06:58 +0530

    feat: Add initial project setup with configuration files, README, and SVG assets

---
+++ frontend-customer/next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['res.cloudinary.com'], // Add Cloudinary domain here
+    },
+  };
+
+export default nextConfig;
+
