21d0e0abce74168e4d0a5e199169a6c3943e0095
SunaliB <basnaya-im21009@stu.kln.ac.lk>
2025-04-25 10:16:50 +0530

    feat: Add global styles and layout structure; implement Home page with Login component

---
+++ frontend-customer/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Login from '@/app/Login/page'
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Login />
+    </div>
+  );
+}
